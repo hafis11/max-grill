4cc156ca9314f04c97120998423fec0f584e2943
Hafis muhammed <frontend.developer.g1b@t-dna.in>
2021-12-07 23:26:30 +0530

    first commit

---
+++ components/menu/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MenuIndex() {
+    return (
+        <div className="h-screen w-screen flex justify-center items-center bg-hero-pattern bg-cover bg-center bg-no-repeat">
+            <h4 className="text-white">hallo</h4>
+        </div>
+    )
+}
+
+export default MenuIndex;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/assets/bg/altumcode-U0tBTn8UR8I-unsplash.jpg')",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
